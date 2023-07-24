98089eca4ef2fa5f97124666067d8eedf956f1e1
Nu7nes <102614883+Nu7nes@users.noreply.github.com>
2023-07-24 14:11:04 -0300

    second commit

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import os
 import shutil
 
 source = 'C:/Users/jessica/Documents/Estudo'
-destination = ''
+destination = 'D:/'
 
 files = os.listdir()
 print(files)
 
-# os.rename('teste.txt', "teste/teste.txt")
-# shutil.copy2('teste.txt', 'teste/teste.txt')
-shutil.make_archive('backup', 'zip', 'teste')
+# os.rename('teste.txt', "teste/teste.txt") mover arquivos
+# shutil.copy2('teste.txt', 'teste/teste.txt') copiar arquivos
+# shutil.make_archive('backup', 'zip', 'teste') compactar arquivos
+
+
+
+
