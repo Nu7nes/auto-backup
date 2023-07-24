1dc18c6519746783a294f319833f1a87c20c6692
Nu7nes <102614883+Nu7nes@users.noreply.github.com>
2023-07-24 16:56:20 -0300

    add tqdm lib

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import os
 import shutil
+import sys
+from tqdm import tqdm
+
+
+def print_with_update(item_path):
+    line_length = 100  # Define o comprimento da linha que você deseja utilizar
+
+    # Preenche a string com espaços adicionais, se necessário
+    item_path_padded = item_path.ljust(line_length)[:line_length]
+
+    # Imprime a linha atualizando o conteúdo anterior
+    sys.stdout.write('      Working on --> ' + item_path_padded + '\r')
+    sys.stdout.flush()
+
 
 directory = 'Estudo'
 source_directory = f'C:/Users/jessica/Documents/{directory}'
 destination_directory = f'D:/{directory}'
 folders_to_ignore = ['node_modules', '.git', '.tmp.driveupload', 'venv']
+
 
 print('     Verifying...')
 if not os.path.exists(destination_directory):
@@ -13,14 +28,14 @@ if not os.path.exists(destination_directory):
 else:
     print("         >>> A pasta já existe.")
 
+
+
 print('     Initializing...')
 def copy_files_with_ignore(src, dst, ignore_folders=[]):
-
-    for item  in os.listdir(src):
+    for item  in tqdm(os.listdir(src), desc='   Copying', unit='item'):
 
         item_path = os.path.join(src, item)
-        print('             Working on --> ',item_path, end='\r')
-
+        
         if os.path.isdir(item_path):
             if item in ignore_folders:
                 continue
@@ -30,6 +45,7 @@ def copy_files_with_ignore(src, dst, ignore_folders=[]):
             copy_files_with_ignore(item_path, dst_item_path, ignore_folders)
         else:
             shutil.copy2(item_path, dst)
+
 
 copy_files_with_ignore(source_directory, destination_directory, folders_to_ignore)
 print(' -----------------------------')
