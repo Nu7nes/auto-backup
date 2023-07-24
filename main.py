b268b92486ed9d2f2ff8d9bccf50f8c7706f9907
Nu7nes <102614883+Nu7nes@users.noreply.github.com>
2023-07-24 16:12:13 -0300

    cmd printing improved

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ source_directory = f'C:/Users/jessica/Documents/{directory}'
 destination_directory = f'D:/{directory}'
 folders_to_ignore = ['node_modules', '.git', '.tmp.driveupload']
 
+print('     Verifying...')
 if not os.path.exists(destination_directory):
     os.makedirs(destination_directory)
-    print("Pasta criada com sucesso!")
+    print("         >>> Pasta criada com sucesso!")
 else:
-    print("A pasta já existe.")
+    print("         >>> A pasta já existe.")
 
-
+print('     Initializing...')
 def copy_files_with_ignore(src, dst, ignore_folders=[]):
-    for item in os.listdir(src):
+
+    for item  in os.listdir(src):
         item_path = os.path.join(src, item)
         if os.path.isdir(item_path):
             if item in ignore_folders:
@@ -24,6 +26,9 @@ def copy_files_with_ignore(src, dst, ignore_folders=[]):
             os.makedirs(dst_item_path, exist_ok=True)
             copy_files_with_ignore(item_path, dst_item_path, ignore_folders)
         else:
+            print('             Copying --> ',item_path, end='\r')
             shutil.copy2(item_path, dst)
 
 copy_files_with_ignore(source_directory, destination_directory, folders_to_ignore)
+print(' -----------------------------')
+print('     Is ok! Backup ready!')
